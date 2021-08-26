bbc7efe6927b1d66efd6586f51364160a8a980f6
TobiBiotex <william7.roelens7@gmail.com>
2021-08-27 00:02:41 +0200

    feat: command register #15

--- util/functions.js
+++ util/functions.js
@@ -2,14 +2,19 @@ const mongoose = require("mongoose");
 const { User } = require("../models/index");
 
 module.exports = client => {
+    /* User */
     client.createUser = async user => {
         const merged = Object.assign({_id: mongoose.Types.ObjectId()}, user);
         const createUser = await new User(merged);
-        createUser.save().then(u => console.log(`Nouvel utilisateur -> ${u.username}`));
+        createUser.save().then(u => console.log(`\x1b[34m[INFO]\x1b[35m[DB]\x1b[0m Nouvel utilisateur : ${u.username}`));
     };
 
     client.getUser = async user => {
-        const data = await User.findOne({userID: user.id});
+        return client.findUserById(user.id);
+    };
+
+    client.findUserById = async id => {
+        const data = await User.findOne({userId: id});
         if (data) return data;
         else return;
     };
@@ -22,4 +27,6 @@ module.exports = client => {
         }
         return data.updateOne(settings);
     };
+
+    /* Guild ? Créer d'autres fichiers de fonctions ? */
 }
