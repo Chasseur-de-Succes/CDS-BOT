029b15038c04e5a68b380ea6aaeb90186033cc41
E.C <emilien.casano@gmail.com>
2021-08-30 19:17:25 +0200

    update: ajout jsdoc pour method appel Steam API
    pour aider à la compréhension et ce que cela retourne

--- util/steam.js
+++ util/steam.js
@@ -2,9 +2,28 @@ const superagent = require('superagent');
 const { STEAM_API_KEY } = require('../config');
 
 module.exports = client => {
+    /**
+     * Cherche et retourne seulement les 10 premiers jeux sur le store qui contient le mot 'gameName'
+     * @param {String} gameName 
+     * @returns Object JSON, au format :
+     * { "total":10,
+     *   "items":[
+     *       {
+     *           "type":"app",
+     *           "name":"Team Fortress 2",
+     *           "id":440,
+     *           "tiny_image":"https:\/\/cdn.akamai.steamstatic.com\/steam\/apps\/440\/capsule_231x87.jpg?t=1592263852",
+     *           "metascore":"92",
+     *           "platforms":{
+     *               "windows":true,
+     *               "mac":true,
+     *               "linux":true
+     *           },
+     *    "streamingvideo":false
+     *   }, ... ]
+     *  }
+     */
     client.findGamesByName = async gameName => {
-        /* const query = args;
-        console.log(query); */
         const search = await superagent.get('https://store.steampowered.com/api/storesearch')
                                         .query({
                                             cc: 'FR',
@@ -15,6 +34,16 @@ module.exports = client => {
         return search.body.items;
     };
 
+   /**
+    * Récupère 50K apps qui ne sont ni des DLC, ni des software, ni des vidéos et ni des hardware
+    * pour récupérer l'array des apps : 'response.body.response.apps;'
+    * @returns Array, au format 
+    * apps: [
+    *        {"appid":10,"name":"Counter-Strike","last_modified":1602535893,"price_change_number":11974414},
+    *        {"appid":20,"name":"Team Fortress Classic","last_modified":1579634708,"price_change_number":11974414},
+    *        ...
+    *    ]
+    */
     client.getAppList = async () => {
         const response = await superagent.get('https://api.steampowered.com/IStoreService/GetAppList/v1/?')
                 .query({
@@ -33,6 +62,12 @@ module.exports = client => {
         return response;
     };
 
+    /**
+     * Récupère les informations d'un jeu Steam en fonction de son appid
+     * // TODO a revoir et à tester
+     * @param {Number} appid 
+     * @returns 
+     */
     client.getAppDetails = async appid => {
         const response = await superagent.get('https://store.steampowered.com/api/appdetails/?')
             .query({
