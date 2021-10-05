e3659d566c07ceb18e64ab9608ae4bd693a1c2ed
TobiBiotex <williamroelens@protonmail.com>
2021-10-05 20:21:48 +0200

    feat: cmd whitelistchannel (en développement)  #4

--- commands/admin/whitelistchannel.js
+++ commands/admin/whitelistchannel.js
@@ -1,11 +1,37 @@
 const { MessageEmbed, Permissions } = require('discord.js');
 
 const { MESSAGES } = require("../../util/constants");
+const { PREFIX } = require("../../config");
 const { check_mark, cross_mark } = require('../../data/emojis.json');
-const { dark_red } = require("../../data/colors.json");
+const { dark_red, night } = require("../../data/colors.json");
 
 module.exports.run = async (client, message, args) => {
     if(!message.member.permissions.has(Permissions.FLAGS.MANAGE_CHANNELS)) return error(`${cross_mark} Vous n'avez pas la permission requise !`);
+
+    if(!args) return error(`${cross_mark} Merci de préciser un argument : \`${PREFIX}whitelistchannel <add | remove | list> [<mention mention>]\``);
+    
+    const guildId = message.guildId;
+
+    if(args[0] == "list") {
+        let configServ = await client.findGuildById(guildId);
+        let whitelist = configServ.whitelistChannel || "none";
+
+        const embed = new MessageEmbed()
+            .color(night)
+            .setTitle("Liste des channels whitlisté")
+            .setDescription(whitelist);
+        message.channel.send({embeds: [embed]});
+
+    } else if(args[0] == "add") {
+        if(!args[1]) return error(`${cross_mark} Merci de mentionner un channel : \`${PREFIX}whitelistchannel <add | remove | list> <mention mention>\``);
+            // AJOUT CHANNEL
+
+    } else if(args[0] == "remove") {
+            // REMOVE CHANNEL
+
+    } else {
+        if(!args[1]) return error(`${cross_mark} Mauvaise utilisation de la commande ! Utilisation attendue : \`${PREFIX}whitelistchannel <add | remove | list> [<mention channel>]\``);
+    }
 
     message.react(check_mark);
 
