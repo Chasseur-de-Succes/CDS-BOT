93f37ddf5809bda3fff5e642f67f61c0cf24081f
TobiBiotex <williamroelens@protonmail.com>
2021-10-08 17:41:04 +0200

    feat: cmd givemoney #27

--- commands/admin/givemoney.js
+++ commands/admin/givemoney.js
@@ -1,12 +1,52 @@
 const { MessageEmbed, Permissions } = require('discord.js');
 
 const { MESSAGES } = require("../../util/constants");
-const { check_mark, cross_mark } = require('../../data/emojis.json');
-const { dark_red } = require("../../data/colors.json");
-const { run } = require('../misc/register');
+const { cross_mark } = require('../../data/emojis.json');
+const { dark_red, green } = require("../../data/colors.json");
+const { PREFIX, MONEY } = require('../../config');
 
 module.exports.run = async (client, message, args) => {
-    return message.channel.send("command en développement !");
+    if(!message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) embedError(`Vous n'avez pas la permission pour exécuter cette commande !`) ;
+
+    if(!message.mentions.users.first()) return embedError(`Merci de mentionner l'utilisateur. Commande attendue : \`${PREFIX}givemoney <user mention> <montant>\``);
+    const user = message.mentions.users.first();
+    const member = message.guild.members.cache.get(user.id);
+    if(!member || !args[1]) return embedError(`Mauvais arguments. Commande attendue : \`${PREFIX}givemoney <user mention> <montant>\`\nRappel: le montant peut être positif comme négatif et uniquement un entier.`);
+
+    let montant;
+    if(Number(args[1])) {
+        montant = Number(args[1]);
+    } else {
+        return embedError(`L'argent rentré : "${args[1]}" n'est pas valide ! Le montant doit être un entier positif ou négatif.`)
+    }
+
+    const dbUser = await client.getUser(member);
+    if(!dbUser) return embedError(`Cette utilisateur n'est pas inscrit !`);
+    
+    let money = dbUser.money;
+    money += montant;
+    if(money < 0) {
+        montant -= money;
+        money = 0;
+    }
+    let msgCustom = montant > 0 ? `à donné` : `à retiré`;
+
+    await client.updateUser(dbUser, { money: money });
+    console.log(`\x1b[31m[WARN] \x1b[0m ${message.author.tag} a effectué la commande admin : ${MESSAGES.COMMANDS.ADMIN.GIVEMONEY.name}`);
+    const embed = new MessageEmbed()
+        .setColor(green)
+        .setDescription(`${message.author} ${msgCustom} **${montant}** ${MONEY} à ${member.user}\nSon argent est désormais de : ${money} ${MONEY}`);
+    message.channel.send({embeds: [embed]});
+
+    // TODO : APPELER EVENT CUSTOM POUR ENVOYER LOG
+
+    function embedError(err) {
+        const embedError = new MessageEmbed()
+            .setColor(dark_red)
+            .setTitle(`${cross_mark} | ${err}`);
+    
+        return message.channel.send({embeds: [embedError]});
+    }
 }
 
 module.exports.help = MESSAGES.COMMANDS.ADMIN.GIVEMONEY;
