bcb61ea86b9ae080bc80b8ffea4e5b8da8f2a913
TobiBiotex <williamroelens@protonmail.com>
2021-12-17 12:50:56 +0100

    update: event guildMemberAdd

--- events/member/guildMemberAdd.js
+++ events/member/guildMemberAdd.js
@@ -1,12 +1,18 @@
-const { CORNFLOWER_BLUE} = require('../../data/colors.json');
+const { CORNFLOWER_BLUE } = require('../../data/colors.json');
 const { MessageEmbed } = require("discord.js");
+const { CHANNEL } = require('../../config');
+const moment = require('moment');
 
 module.exports = async (client, member) => {
+    const user = client.users.cache.get(member.id);
     const embed = new MessageEmbed()
-    .setColor(CORNFLOWER_BLUE)
-    .setTitle(`Nouveau membre`)
-    .setDescription(`<@${member.id}>`)
-    .addField("ID", member.id);
+        .setColor(CORNFLOWER_BLUE)
+        .setTitle(`Nouveau membre`)
+        .setDescription(`<@${member.id}>`)
+        .addFields(
+            {name: "Âge du compte", value: `<t:${Math.floor(user.createdTimestamp / 1000)}:R>`},
+            {name: "ID", value: `${member.id}`},
+        );
 
-    client.channels.cache.get('879395583390003281').send({embeds: [embed]});
+    client.channels.cache.get(CHANNEL.WELCOME).send({embeds: [embed]});
 }
