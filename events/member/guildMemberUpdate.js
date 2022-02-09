d43b280f95c5d72cdc28568c9357eea7fa21e05f
TobiBiotex <williamroelens@protonmail.com>
2022-02-09 15:16:01 +0100

    fix: log nickname update

--- events/member/guildMemberUpdate.js
+++ events/member/guildMemberUpdate.js
@@ -3,14 +3,16 @@ const { MessageEmbed } = require("discord.js");
 const { CHANNEL } = require('../../config');
 
 module.exports = async (client, oldUser, newUser) => {
-    let oldNickname = oldUser.nickname || '_Aucun_';
-    let newNickname = newUser.nickname ||'_Aucun_';
-    const embed = new MessageEmbed()
-        .setColor(ORANGE)
-        .setTitle(`Surnom modifier`)
-        .setDescription(`<@${newUser.id}>\nAncien surnom: ${oldNickname}\nNouveau surnom: ${newNickname}`)
-        .setFooter(`ID: ${newUser.id}`)
-        .setTimestamp();
+    if(oldUser.nickname != newUser.nickname) {
+        let oldNickname = oldUser.nickname || '_Aucun_';
+        let newNickname = newUser.nickname ||'_Aucun_';
+        const embed = new MessageEmbed()
+            .setColor(ORANGE)
+            .setTitle(`Surnom modifier`)
+            .setDescription(`<@${newUser.id}>\nAncien surnom: ${oldNickname}\nNouveau surnom: ${newNickname}`)
+            .setFooter(`ID: ${newUser.id}`)
+            .setTimestamp();
 
-    client.channels.cache.get(CHANNEL.LOGS).send({embeds: [embed]});
+        client.channels.cache.get(CHANNEL.LOGS).send({embeds: [embed]});
+    }
 }
