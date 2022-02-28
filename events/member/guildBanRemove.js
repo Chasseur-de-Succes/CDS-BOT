2dac1f62d6f092b526cffbbaf9d151ad07ce4e06
TobiBiotex <williamroelens@protonmail.com>
2022-02-28 13:12:34 +0100

    feat: log ban & unban #31

---
+++ events/member/guildBanRemove.js
@@ -0,0 +1,18 @@
+const { GREEN } = require('../../data/colors.json');
+const { MessageEmbed } = require("discord.js");
+const { CHANNEL } = require('../../config');
+
+module.exports = async (client, user) => {
+    const embedLog = new MessageEmbed()
+        .setColor(GREEN)
+        .setAuthor(`Membre débanni`, user.user.displayAvatarURL({dynamic: true, size: 4096, format: 'png'}))
+        .setThumbnail(user.user.displayAvatarURL({dynamic : true, size: 4096, format: 'png'}))
+        .setDescription(`<@${user.user.id}>\n`)
+        .addFields(
+            {name: "Membre", value: `\`${user.user.tag}\` - <@${user.user.id}>`, inline: true},
+        )
+        .setFooter(`ID: ${user.user.id}`)
+        .setTimestamp();
+
+    client.channels.cache.get(CHANNEL.LOGS).send({embeds: [embedLog]});
+}
