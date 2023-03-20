6b5750d518257a48b0d7e73dcde82a13e6c422e5
TobiBiotex <williamroelens@protonmail.com>
2023-03-20 11:20:39 +0100

    deletion old commands

--- models/guildConfig.js
+++ models/guildConfig.js
@@ -3,9 +3,6 @@ const mongoose = require('mongoose');
 const guildConfigSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     guildId: String,
-    whitelistChannel : [{
-        "type": String
-    }],
     channels : {
         welcome: String,
         role: String,
