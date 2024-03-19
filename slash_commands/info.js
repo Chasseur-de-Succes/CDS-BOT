57335d75f818b8d7a0a6552c65e7b3a6c8c591fb
0xN0x <n0x@n0x.cc>
2024-03-19 20:52:49 +0100

    refactor: remove useless cpuStat from info command

--- slash_commands/info.js
+++ slash_commands/info.js
@@ -7,43 +7,38 @@ module.exports = {
         .setName("info")
         .setDMPermission(false)
         .setDescription("Informations sur le bot"),
+
     async execute(interaction) {
         const client = interaction.client;
         const botIcon = client.user.displayAvatarURL();
         const VERSION = process.env.VERSION;
-        
-        cpuStat.usagePercent(function (err, percent, second){
-            if(err) {
-                logger.error(err);
-            }
 
-            let developers = ""
-            for (const devID of process.env.DEVELOPERS.split(',')) {
-                const dev = client.users.cache.get(devID);
-                if (dev) developers += `- ${dev.tag}\n`
-            }
-    
-            const timeUp = client.uptime;
-            const embedInfo = new EmbedBuilder()
-                .setColor(GREEN)
-                .setTitle(client.user.username)
-                .setDescription(`La version actuelle est ${VERSION}`)
-                .setThumbnail(botIcon)
-                .addFields(
-                    { name: 'Développeur', value: developers },
-                    { name: 'Créé le', value: `<t:${Math.floor(client.user.createdTimestamp / 1000)}:f>` },
-                    { name: 'Langage', value: `JavaScript`, inline: true},
-                    { name: 'Library', value: `discord.js`, inline: true },
-                    { name: 'Discord.js', value: `v${version}`, inline: true },
-                    { name: 'Uptime', value: 
-                        (Math.round(timeUp / (1000 * 60 * 60 * 24))) + " days, " 
-                        + (Math.round(timeUp / (1000 * 60 * 60)) % 24) + " hrs, " 
-                        + (Math.round(timeUp / (1000 * 60)) % 60) + " mins and " 
-                        + (Math.round(timeUp / 1000) % 60) + " sec" },
-                )
-                .setFooter({ text: 'Demandé par ' + interaction.user.username});
-    
-            return interaction.reply({embeds: [embedInfo]});
-        })
+        let developers = ""
+        for (const devID of process.env.DEVELOPERS.split(',')) {
+            const dev = client.users.cache.get(devID);
+            if (dev) developers += `- ${dev.tag}\n`
+        }
+
+        const timeUp = client.uptime;
+        const embedInfo = new EmbedBuilder()
+            .setColor(GREEN)
+            .setTitle(client.user.username)
+            .setDescription(`La version actuelle est ${VERSION}`)
+            .setThumbnail(botIcon)
+            .addFields(
+                { name: 'Développeur', value: developers },
+                { name: 'Créé le', value: `<t:${Math.floor(client.user.createdTimestamp / 1000)}:f>` },
+                { name: 'Langage', value: `JavaScript`, inline: true},
+                { name: 'Library', value: `discord.js`, inline: true },
+                { name: 'Discord.js', value: `v${version}`, inline: true },
+                { name: 'Uptime', value:
+                    (Math.round(timeUp / (1000 * 60 * 60 * 24))) + " days, "
+                    + (Math.round(timeUp / (1000 * 60 * 60)) % 24) + " hrs, "
+                    + (Math.round(timeUp / (1000 * 60)) % 60) + " mins and "
+                    + (Math.round(timeUp / 1000) % 60) + " sec" },
+            )
+            .setFooter({ text: 'Demandé par ' + interaction.user.username});
+
+        return interaction.reply({embeds: [embedInfo]});
     },
 }
