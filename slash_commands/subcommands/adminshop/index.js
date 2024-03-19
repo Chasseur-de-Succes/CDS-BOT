7ac3be6f3d38590d7118def4229883b10d357d96
0xN0x <n0x@n0x.cc>
2024-03-19 20:04:19 +0100

    refactor: split adminshop command

---
+++ slash_commands/subcommands/adminshop/index.js
@@ -0,0 +1,9 @@
+const { cancel } = require("./cancel");
+const { deleteItem } = require("./delete");
+const { refund } = require("./refund");
+
+module.exports = {
+    cancel,
+    deleteItem,
+    refund,
+};
