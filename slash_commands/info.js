b8e4e76471e2c063adde319ab12cd256f8e6302e
0xN0x <n0x@n0x.cc>
2024-03-20 00:41:28 +0100

    refactor: replace quote with double quotes

--- slash_commands/info.js
+++ slash_commands/info.js
@@ -1,6 +1,5 @@
 const { version, EmbedBuilder, SlashCommandBuilder } = require("discord.js");
 const { GREEN } = require("../data/colors.json");
-const cpuStat = require("cpu-stat");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -14,7 +13,7 @@ module.exports = {
         const VERSION = process.env.VERSION;
 
         let developers = ""
-        for (const devID of process.env.DEVELOPERS.split(',')) {
+        for (const devID of process.env.DEVELOPERS.split(",")) {
             const dev = client.users.cache.get(devID);
             if (dev) developers += `- ${dev.tag}\n`
         }
@@ -26,18 +25,18 @@ module.exports = {
             .setDescription(`La version actuelle est ${VERSION}`)
             .setThumbnail(botIcon)
             .addFields(
-                { name: 'Développeurs', value: developers },
-                { name: 'Créé le', value: `<t:${Math.floor(client.user.createdTimestamp / 1000)}:f>` },
-                { name: 'Langage', value: `JavaScript`, inline: true},
-                { name: 'Library', value: `discord.js`, inline: true },
-                { name: 'Discord.js', value: `v${version}`, inline: true },
-                { name: 'Uptime', value:
-                    (Math.round(timeUp / (1000 * 60 * 60 * 24))) + " days, "
-                    + (Math.round(timeUp / (1000 * 60 * 60)) % 24) + " hrs, "
-                    + (Math.round(timeUp / (1000 * 60)) % 60) + " mins and "
-                    + (Math.round(timeUp / 1000) % 60) + " sec" },
+                { name: "Développeurs", value: developers },
+                { name: "Créé le", value: `<t:${Math.floor(client.user.createdTimestamp / 1000)}:f>` },
+                { name: "Langage", value: "JavaScript", inline: true},
+                { name: "Library", value: "discord.js", inline: true },
+                { name: "Discord.js", value: `v${version}`, inline: true },
+                { name: "Uptime", value:
+                    `${(Math.round(timeUp / (1000 * 60 * 60 * 24)))} days, `
+                    + `${(Math.round(timeUp / (1000 * 60 * 60)) % 24)} hours, `
+                    + `${(Math.round(timeUp / (1000 * 60)) % 60)} mins and `
+                    + `${(Math.round(timeUp / 1000) % 60)} secs` },
             )
-            .setFooter({ text: 'Demandé par ' + interaction.user.username});
+            .setFooter({ text: `Demandé par ${interaction.user.username}` });
 
         return interaction.reply({embeds: [embedInfo]});
     },
