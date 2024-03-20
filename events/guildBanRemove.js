4bb8644f34b4e614d35918c3e9ad565d58f86221
rick <33371456+rick-gnous@users.noreply.github.com>
2024-03-21 00:13:32 +0100

    refactor: use toString()

--- events/guildBanRemove.js
+++ events/guildBanRemove.js
@@ -9,7 +9,7 @@ module.exports = {
             .setColor(GREEN)
             .setAuthor(`Membre débanni`, ban.user.displayAvatarURL({dynamic: true, size: 4096, format: 'png'}))
             .setThumbnail(ban.user.displayAvatarURL({dynamic : true, size: 4096, format: 'png'}))
-            .setDescription(`${ban.user}`)
+            .setDescription(ban.user.toString())
             .addFields(
                 {name: "Membre", value: `\`${ban.user.tag}\` - ${ban.user}`, inline: true},
             )
