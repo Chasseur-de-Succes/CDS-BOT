a28d04d6a90ff34e4612ba1f5263d515fcf60aad
rick <33371456+rick-gnous@users.noreply.github.com>
2024-03-21 00:08:44 +0100

    refactor: remove useless \n

--- events/guildBanRemove.js
+++ events/guildBanRemove.js
@@ -9,7 +9,7 @@ module.exports = {
             .setColor(GREEN)
             .setAuthor(`Membre débanni`, ban.user.displayAvatarURL({dynamic: true, size: 4096, format: 'png'}))
             .setThumbnail(ban.user.displayAvatarURL({dynamic : true, size: 4096, format: 'png'}))
-            .setDescription(`${ban.user}\n`)
+            .setDescription(`${ban.user}`)
             .addFields(
                 {name: "Membre", value: `\`${ban.user.tag}\` - ${ban.user}`, inline: true},
             )
