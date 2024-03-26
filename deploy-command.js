a1fce756f1964d9a8451c5e7edf7b9828ef33ac5
rick <rick@gnous.eu>
2024-03-26 02:16:15 +0100

    refactor: fix linter

--- deploy-command.js
+++ deploy-command.js
@@ -1,22 +1,24 @@
-const { REST, Routes } = require('discord.js');
-const fs = require('node:fs');
+const { REST, Routes } = require("discord.js");
+const fs = require("node:fs");
 require("dotenv").config();
 
 // recup config
-process.env.cli
+process.env.cli;
 
 const commands = [];
 // Grab all the command files from the commands directory you created earlier
-const commandFiles = fs.readdirSync('./slash_commands/').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+    .readdirSync("./slash_commands/")
+    .filter((file) => file.endsWith(".js"));
 
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
-	const command = require(`./slash_commands/${file}`);
-	commands.push(command.data.toJSON());
+    const command = require(`./slash_commands/${file}`);
+    commands.push(command.data.toJSON());
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 // for guild-based commands
 // DISCORD CDS
@@ -31,19 +33,23 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 // and deploy your commands!
 (async () => {
-	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+    try {
+        console.log(
+            `Started refreshing ${commands.length} application (/) commands.`,
+        );
 
-		// The put method is used to fully refresh all commands in the guild with the current set
-		const data = await rest.put(
+        // The put method is used to fully refresh all commands in the guild with the current set
+        const data = await rest.put(
             // GLOBAL !
-			Routes.applicationCommands(process.env.CLIENTID),
-			{ body: commands },
-		);
+            Routes.applicationCommands(process.env.CLIENTID),
+            { body: commands },
+        );
 
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-	} catch (error) {
-		// And of course, make sure you catch and log any errors!
-		console.error(error);
-	}
+        console.log(
+            `Successfully reloaded ${data.length} application (/) commands.`,
+        );
+    } catch (error) {
+        // And of course, make sure you catch and log any errors!
+        console.error(error);
+    }
 })();
