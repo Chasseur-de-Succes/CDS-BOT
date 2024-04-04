0893088b12c0f0fc27ef6525051c43349384a2bd
E.C <emilien.casano@gmail.com>
2024-04-04 23:00:17 +0200

    refactor: TODOs models

--- models/shop/customItemShop.js
+++ models/shop/customItemShop.js
@@ -1,10 +1,4 @@
 const mongoose = require("mongoose");
 const ItemShop = require("./itemShop");
 
-// TODO shop custom
-module.exports = ItemShop.discriminator(
-    "CustomItem",
-    new mongoose.Schema({
-        // TODO
-    }),
-);
+module.exports = ItemShop.discriminator("CustomItem", new mongoose.Schema({}));
