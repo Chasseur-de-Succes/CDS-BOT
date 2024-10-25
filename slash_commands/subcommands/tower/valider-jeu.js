2797898a6fc28c985d42f37930fd9ed831cbc4dd
Emilien Casano <emilien.casano@univ-lorraine.fr>
2024-10-25 22:49:10 +0200

    ft: tower event - valider : embed + log

--- slash_commands/subcommands/tower/valider-jeu.js
+++ slash_commands/subcommands/tower/valider-jeu.js
@@ -1,7 +1,43 @@
 const { createError } = require("../../../util/envoiMsg");
-const { HIDDEN_BOSS, BOSS, ETAGE_PAR_PALIER, MAX_ETAGE, ASCII_FIRST } = require("../../../data/event/tower/constants.json")
-const { EventBoss, GuildConfig} = require("../../../models");
+const {
+  HIDDEN_BOSS,
+  BOSS,
+  ETAGE_PAR_PALIER,
+  MAX_ETAGE,
+  ASCII_FIRST,
+  ASCII_PALIER,
+  ASCII_BOSS_FIRST_TIME,
+  ASCII_100,
+  ASCII_NOT_100,
+  ASCII_HIDDEN_BOSS_FIRST_TIME,
+  ASCII_END,
+  PRIVATE_JOKES
+} = require("../../../data/event/tower/constants.json")
+const { EventBoss, GuildConfig, User} = require("../../../models");
 const { EmbedBuilder } = require("discord.js");
+
+// Récupère une private joke aléatoirement
+function getRandomPrivateJokes() {
+  return PRIVATE_JOKES[Math.floor(Math.random() * PRIVATE_JOKES.length)];
+}
+
+// Calcul le pourcentage de vie restant du boss donné, retourne une suite d'émoji
+function displayHealth(boss) {
+  const totalHP = 5;
+  const filledRatio = (boss.hp / boss.maxHp) * totalHP; // Ratio de cases pleines
+  const filledHP = Math.floor(filledRatio); // Cases totalement remplies (arrondi inférieur)
+  const hasIntermediate = filledRatio > filledHP; // Vérifie s'il reste une fraction pour une case intermédiaire
+  const emptyHP = totalHP - filledHP - (hasIntermediate ? 1 : 0); // Cases vides
+
+  // Sélection des émojis de couleur selon le ratio de vie
+  let filledEmoji = '🟩'; // Par défaut, plein de vie
+  if (boss.hp / boss.maxHp <= 0.3) filledEmoji = '🟥'; // Faible santé
+  else if (boss.hp / boss.maxHp <= 0.6) filledEmoji = '🟨'; // Santé moyenne
+  const intermediateEmoji = '🟧'; // Émoji intermédiaire
+  const emptyEmoji = '⬜'; // Cases vides plus douces
+
+  return `${filledEmoji.repeat(filledHP)}${hasIntermediate ? intermediateEmoji : ''}${emptyEmoji.repeat(emptyHP)}`;
+}
 
 // Créer un boss si aucun n'existe
 async function createBoss(season, isHiddenBoss) {
@@ -32,13 +68,11 @@ async function createEmbed(option) {
 
 const validerJeu = async (interaction, options) => {
   // TODO autoriser seulement si dans bon salon ?
+    // TODO => save salon dans config..
   const guildId = interaction.guildId;
   let appid = options.get("appid")?.value;
   appid = !appid ? options.get("jeu")?.value : appid;
 
-  if (!appid) {
-    return await interaction.reply("Tu dois spécifier au moins un appID ou chercher le jeu que tu as complété.");
-  }
   const author = interaction.member;
   const client = interaction.client;
 
@@ -54,90 +88,183 @@ const validerJeu = async (interaction, options) => {
       ],
     });
   }
+  // si pas inscrit
+  if (typeof userDb.event.tower.startDate === 'undefined') {
+    return await interaction.reply({ content: 'Tu dois d\'abord t\'inscrire !', ephemeral: true });
+  }
 
+  if (!appid) {
+    return await interaction.reply("Tu dois spécifier au moins un appID ou chercher le jeu que tu as complété.");
+  }
+
+  // récupère la saison courante, si n'existe pas, on commence à 0 et on save dans la config
+  const guild = await GuildConfig.findOne({ guildId: guildId });
+
+  if (typeof guild.event.tower.currentSeason === 'undefined') {
+    logger.info('.. création attribut currentSeason pour config');
+    guild.event.tower.currentSeason = 0;
+    await guild.save();
+  }
+
+  const season = guild.event.tower.currentSeason;
+
+  // teste si le boss est en vie, sinon on skip
+  const allBossDead = await EventBoss.exists({
+    $and: [
+      {
+        season: season,
+        hp: {$eq: 0},
+        hidden: false
+      }, {
+        season: season,
+        hp: {$eq: 0},
+        hidden: true
+    }]
+  });
+
+  if (allBossDead) {
+    logger.info('.. tous les boss sont DEAD ..');
+    return await interaction.reply({
+      content: 'L\'événement est terminé ! Revenez peut être plus tard..',
+      ephemeral: true
+    });
+  }
+
+  // récupération des infos des succès sur le jeu sélectionné via Steam
   const steamId = userDb.steamId;
-
-  // TODO tester si le boss est en vie, sinon RAF
-  const season = await GuildConfig.find({ guildId: guildId }).sort({ season: -1 }).limit(1)?.season || 0;
-  // TODO gestion erreur
+  // TODO gestion erreur connexion ?
   const { error, gameName, hasAllAchievements } = await client.hasAllAchievementsUnlocked(steamId, appid);
 
   if (error) {
     // Recup nom du jeu, si présent dans la bdd
     const gameDb = await client.findGameByAppid(appid);
-    // TODO ephemeral
-    return await interaction.reply(`${gameDb?.name} n'a même pas de succès..`);
+    // TODO si gameDb non trouvé
+    return await interaction.reply({
+      content: `${gameDb?.name} n'a même pas de succès..`,
+      ephemeral: true
+    });
   }
 
   if (hasAllAchievements) {
     // Vérifier si l'utilisateur a déjà 100% le jeu
-    // TODO
-    // if (userDb.event.tower.completedGames.includes(appid)) {
-    // // TODO ephemeral
-    //   return await interaction.reply("Tu as déjà utilisé ce jeu.. ce n'est pas très efficace.");
-    // }
+    if (userDb.event.tower.completedGames.includes(appid)) {
+      return await interaction.reply({
+        content: "Tu as déjà utilisé ce jeu.. ce n'est pas très efficace.",
+        ephemeral: true
+      });
+    }
 
     userDb.event.tower.etage += 1;  // On monte d'un étage
-    // userDb.event.tower.completedGames.push(appid);  // Ajouter l'appId aux jeux déjà 100%
+    userDb.event.tower.completedGames.push(appid);  // Ajouter l'appId aux jeux déjà 100%
     await userDb.save();
 
     // Si l'utilisateur n'est pas encore arrivé au boss
-    if (userDb.event.tower.etage < MAX_ETAGE) {
-      // TODO ephemeral
-      // await interaction.reply(`${author} as complété **${gameName}** et monte d'un étage ! (Étage ${userDb.event.tower.etage})`);
-      // TODO embed general, mais juste avec le nom du jeu
-
+    if (userDb.event.tower.etage <= MAX_ETAGE) {
+      // 1er étage franchi (1 jeu complété)
       if (userDb.event.tower.etage === 1) {
+        logger.info({
+          prefix: 'EVENT TOWER',
+          message: `${author.nickname} atteint 1er étage ..`
+        });
         // 1er message d'intro
-        // TODO mettre message ASCII autre part pour + de lisibiltié
         return interaction.reply({
           embeds: [await createEmbed({
             title: `🏆 ${gameName} terminé !`,
             url: `https://store.steampowered.com/app/${appid}/`,
-            desc: `En complétant **${gameName}**, ${author} ressent assez d'énergie pour pénétrer dans la tour, et gravir les escalier, pour atteindre le premier **étage** !
+            desc: `En complétant **${gameName}**, ${author} ressent assez d'énergie pour pénétrer dans la tour, et gravir les escaliers, pour atteindre le premier **étage** !
 ${ASCII_FIRST}`,
             color: '#1cff00',
             footer: {
-              text: 'Étage 1'
+              text: 'Étage 1/??'
+            }
+          })],
+          ephemeral: true
+        });
+      }
+
+      // Si l'utilisateur est arrivé à l'étage du boss (MAX_ETAGE jeux complétés)
+      if (userDb.event.tower.etage === MAX_ETAGE) {
+        const bossCreated = await EventBoss.exists({ season: season, hidden: false });
+
+        // Si boss pas créé, on le crée
+        if (!bossCreated) {
+          logger.info({
+            prefix: 'EVENT TOWER',
+            message: `${author.nickname} atteint dernier palier, création 1er boss..`
+          });
+          const newBoss = await createBoss(season, false);
+          return interaction.reply({
+            embeds: [await createEmbed({
+              title: `🏆 ${gameName} terminé !`,
+              url: `https://store.steampowered.com/app/${appid}/`,
+              desc: `${author} a atteint le **palier ${userDb.event.tower.etage / ETAGE_PAR_PALIER}** et est arrivé au sommet de la tour !!
+${author} aperçoit au loin une ombre menaçante.\n
+En se rapprochant, ${author} reconnait très clairement le cupide \`${newBoss.name}\`..\n
+Attention, il fonce droit sur vous !!
+${ASCII_BOSS_FIRST_TIME}`,
+              color: '#ff0000',
+              footer: {
+                text: `"Tiens, un jeu gratuit !" 😈`
+              }
+            })]
+          });
+        }
+
+        const hiddenBossCreated = await EventBoss.exists({ season: season, hidden: true });
+
+        // Si boss caché pas encore créé, on rejoint le combat contre le 1er
+        if (!hiddenBossCreated) {
+          return interaction.reply({
+            embeds: [await createEmbed({
+              title: `🏆 ${gameName} terminé !`,
+              url: `https://store.steampowered.com/app/${appid}/`,
+              desc: `${author} a atteint le **palier ${userDb.event.tower.etage / ETAGE_PAR_PALIER}** et est arrivé au sommet de la tour !!
+${author} aperçoit au loin d'autres joueurs menant une rude bataille..
+${author} prends part au combat !`,
+              color: '#ff0000',
+              footer: {
+                text: 'Enfin en haut !'
+              }
+            })]
+          });
+        }
+
+        // Si boss caché créé, le 1er est mort, on rejoint le combat contre le 2ème
+        const deadBoss = await EventBoss.findOne({ season: season, hp: { $eq: 0 }, hidden: false });
+        const currentBoss = await EventBoss.findOne({ season: season, hp: { $ne: 0 } });
+        return interaction.reply({
+          embeds: [await createEmbed({
+            title: `🏆 ${gameName} terminé !`,
+            url: `https://store.steampowered.com/app/${appid}/`,
+            desc: `${author} a atteint le **palier ${userDb.event.tower.etage / ETAGE_PAR_PALIER}** et est arrivé au sommet de la tour !!
+Mais ${author} trébuche sur le cadavre de \`${deadBoss.name}\`...
+En se relevant, ${author} voit ses coéquipiers faire face au grand \`${currentBoss.name}\`\n
+${author} prends part au combat !`,
+            color: '#ff00fc',
+            footer: {
+              text: 'Mieux vaux tard que jamais'
             }
           })]
         });
       }
 
-      // Vérifier si l'utilisateur atteint un nouveau palier (10 étages)
+      // Vérifier si l'utilisateur atteint un nouveau palier
       if (userDb.event.tower.etage % ETAGE_PAR_PALIER === 0) {
-        // TODO message général vers salon
-        // TODO revoir message
-        await interaction.followUp(`${author} passes au palier **${userDb.event.tower.etage / ETAGE_PAR_PALIER}** !`);
+        return interaction.reply({
+          embeds: [await createEmbed({
+            title: `🏆 ${gameName} terminé !`,
+            url: `https://store.steampowered.com/app/${appid}/`,
+            desc: `En complétant **${gameName}**, ${author} arrive au **palier ${userDb.event.tower.etage / ETAGE_PAR_PALIER}** !
+            Ce palier est vide.. les escaliers montent toujours et les bruits sont de plus en plus oppressants.
+${ASCII_PALIER}`,
+            color: '#1cff00',
+            footer: {
+              text: `Étage ${userDb.event.tower.etage}/??, Palier ${userDb.event.tower.etage / ETAGE_PAR_PALIER}/??`
+            }
+          })]
+        });
       }
 
-      // Si l'utilisateur est arrivé à l'étage du boss
-      if (userDb.event.tower.etage === MAX_ETAGE) {
-        // TODO message général vers salon
-        const bossCreated = await EventBoss.exists({ season: season, hidden: false });
-        // Si boss pas créé, on le crée
-        if (!bossCreated) {
-          const newBoss = await createBoss(season, false);
-          await interaction.followUp(`${author} est arrivé au sommet de la tour ! ${author} aperçoit au loin une ombre menaçante.\n
-            En se rapprochant, ${author} reconnait très clairement le cupide \`${newBoss.name}\`..\n
-            Attention, il fonce droit sur vous !!`);
-        } else {
-          const hiddenBossCreated = await EventBoss.exists({ season: season, hidden: true });
-
-          // Si boss caché créé ou pas encore, on rejoint le combat contre le 1er ou 2eme
-          if (!hiddenBossCreated) {
-            await interaction.followUp(`${author} est arrivé au sommet de la tour, la bataille fait rage.\n
-              ${author} prends part au combat !`);
-          } else {
-            const deadBoss = await EventBoss.findOne({ season: season, hp: { $ne: 0 }, hidden: false });
-            await interaction.followUp(`${author} est arrivé au sommet de la tour, en trébuchant sur le corps de \`${deadBoss.name}\`.\n
-              En se relevant, ${author} voit ses coéquipiers faire face au grand \`Haiepique Fayl\`\n
-              ${author} prends part au combat !`);
-          }
-        }
-
-        // TODO ASCII ? MessageEmbed avec image + "barre de vie" ?
-      }
     } else {
       // Récupère le boss courant non mort
       const currentBoss = await EventBoss.findOne({ season: season, hp: { $ne: 0 } });
@@ -148,32 +275,129 @@ ${ASCII_FIRST}`,
       await userDb.save();
 
       currentBoss.hp -= damage;
-      if (currentBoss.hp <= 0) {
-        if (currentBoss.hidden) {
-          // TODO si boss caché meurt, on arrête TOUT et on backup la saison
-          await interaction.reply(`${author} a tué \`${currentBoss.name}\` !
-          Le calme est revenu et vous êtes maintenant seul. C'est la fin..`);
-
-        } else {
-          // - si 1er boss dead, gestion du boss caché
-          const hiddenBoss = await createBoss(season, true);
-
-          await interaction.reply(`${author} a tué \`${currentBoss.name}\` !
-          Alors que son corps tombe à terre, ${author} entends grogner au loin..
-          C'est \`${hiddenBoss.name}\`, son acolyte, qui bondit et qui veut venger son maître !`);
-
-        }
-      } else {
-        await interaction.reply(`${author} a complété **${gameName}** et inflige ${damage} de damages ! Et paf !`);
-      }
-
       await currentBoss.save();
 
+      if (currentBoss.hp <= 0) {
+        if (currentBoss.hidden) {
+          logger.info({
+            prefix: 'EVENT TOWER',
+            message: `${author.nickname} tue boss caché, on backup les infos ..`
+          });
+          // si boss caché meurt, on arrête TOUT et on backup la saison
+          await endSeason(season);
+
+          return interaction.reply({
+            embeds: [await createEmbed({
+              title: `🏆 ${gameName} terminé !`,
+              url: `https://store.steampowered.com/app/${appid}/`,
+              desc: `En complétant **${gameName}**, ${author} porte le coup fatal à \`${currentBoss.name}\`!! Bravo !
+Le calme est revenu en haut de cette tour. Vous pouvez vous reposer après cette lutte acharnée.
+C'est la fin..
+${ASCII_END}`,
+              color: '#ff00fc',
+              footer: {
+                text: 'C\'est trop calme..'
+              }
+            })]
+          });
+        }
+
+        // - si 1er boss dead, gestion du boss caché
+        logger.info({
+          prefix: 'EVENT TOWER',
+          message: `${author.nickname} a tué le boss, création boss caché ..`
+        });
+        const hiddenBoss = await createBoss(season, true);
+
+        return interaction.reply({
+          embeds: [await createEmbed({
+            title: `🏆 ${gameName} terminé !`,
+            url: `https://store.steampowered.com/app/${appid}/`,
+            desc: `En complétant **${gameName}**, ${author} porte le coup fatal à \`${currentBoss.name}\`! Bravo !
+Alors que son corps tombe à terre, ${author} entends grogner au loin..
+
+C'est \`${hiddenBoss.name}\`, son acolyte, qui bondit et qui veut venger son maître !
+${ASCII_HIDDEN_BOSS_FIRST_TIME}`,
+            color: '#ff00fc',
+            footer: {
+              text: 'Il n\'a pas l\'air commode'
+            }
+          })]
+        });
+      }
+
+      // Boss toujours en vie
+      const embed = await createEmbed({
+        title: `🏆 ${gameName} terminé !`,
+        url: `https://store.steampowered.com/app/${appid}/`,
+        desc: `En complétant **${gameName}**, ${author} inflige **${damage} point de dégats** à \`${currentBoss.name}\`!
+${ASCII_100}`,
+        color: '#ff00fc',
+        footer: {
+          text: `${getRandomPrivateJokes()}`
+        }
+      });
+      embed.addFields(
+        {
+          name: `${currentBoss.hp}/${currentBoss.maxHp}`,
+          value: `${displayHealth(currentBoss)}`
+        }
+      )
+
+      return interaction.reply({
+        embeds: [embed],
+        ephemeral: true
+      });
     }
   } else {
-    // TODO MessageEmbed
-    await interaction.reply(`Tu n'as pas encore complété **${gameName}**..`);
+    return interaction.reply({
+      embeds: [await createEmbed({
+        title: `🛑 Tu n'as pas encore complété ${gameName}..`,
+        url: `https://store.steampowered.com/app/${appid}/`,
+        desc: `Il semblerait que tu n'es pas eu tous les succès de **${gameName}**..
+${ASCII_NOT_100}`,
+        color: '#0019ff',
+        footer: {
+          text: 'C\'est une erreur ? Oups.. contacte un admin !'
+        }
+      })],
+      ephemeral: true
+    });
   }
+}
+
+async function endSeason(seasonNumber) {
+  logger.info({
+    prefix: 'EVENT TOWER',
+    message: `fin de la saison ${seasonNumber} ..`
+  });
+  // Récupérer tous les utilisateurs qui ont participé
+  const users = await User.find({ 'event.tower.startDate': { $exists: true } });
+
+  // Sauvegarder les informations de la saison actuelle pour chaque utilisateur
+  const endDate = Date.now();
+  for (const user of users) {
+    await endSeasonForUser(user, endDate, seasonNumber);
+  }
+}
+
+async function endSeasonForUser(user, endDate, seasonNumber) {
+  // Sauvegarder les données de la saison actuelle dans l'historique
+  user.event.tower.seasonHistory.push({
+    seasonNumber: seasonNumber,
+    endDate: endDate,
+    maxEtage: user.event.tower.etage,
+    totalDamage: user.event.tower.totalDamage
+  });
+
+  // Réinitialiser les données pour la nouvelle saison
+  user.event.tower.startDate = undefined;
+  user.event.tower.etage = 1;
+  user.event.tower.totalDamage = 0;
+  // user.completedGames = [];
+  // user.season = seasonNumber + 1;
+
+  await user.save();
 }
 
 exports.validerJeu = validerJeu;
