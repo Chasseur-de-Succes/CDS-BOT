6b181d5d1e6f633da1a59dab08a3906bbb5c0042
TobiBiotex <williamroelens@protonmail.com>
2025-11-07 18:43:19 +0100

    Fix: 3s delay command group transfert

--- slash_commands/subcommands/group/transfert.js
+++ slash_commands/subcommands/group/transfert.js
@@ -12,11 +12,13 @@ const transfert = async (interaction, options) => {
 
     const isAdmin = author.permissions.has(PermissionFlagsBits.Administrator);
 
+    await interaction.deferReply();
+
     // Test si le capitaine est inscrit
     const authorDb = await client.getUser(author);
     if (!authorDb) {
         // Si pas dans la BDD
-        return interaction.reply({
+        return interaction.editReply({
             embeds: [
                 createError(
                     `${author.user.tag} n'a pas encore de compte ! Pour s'enregistrer : \`/register\``,
@@ -26,7 +28,7 @@ const transfert = async (interaction, options) => {
     }
     const newCaptainDb = await client.getUser(newCaptain);
     if (!newCaptainDb) {
-        return interaction.reply({
+        return interaction.editReply({
             embeds: [
                 createError(
                     `${newCaptain} n'a pas de compte ! Merci de t'enregistrer avec la commande : \`/register\``,
@@ -38,14 +40,14 @@ const transfert = async (interaction, options) => {
     // Récupération du groupe
     const grp = await client.findGroupByName(grpName);
     if (!grp) {
-        return interaction.reply({
+        return interaction.editReply({
             embeds: [createError(`Le groupe **${grpName}** n'existe pas !`)],
         });
     }
 
     // Si l'auteur n'est pas admin et n'est pas capitaine
     if (!isAdmin && !grp.captain._id.equals(authorDb._id)) {
-        return interaction.reply({
+        return interaction.editReply({
             embeds: [
                 createError(`Tu n'es pas capitaine du groupe **${grpName}** !`),
             ],
@@ -55,7 +57,7 @@ const transfert = async (interaction, options) => {
     // si le nouveau capitaine fait parti du groupe
     const memberGrp = grp.members.find((u) => u._id.equals(newCaptainDb._id));
     if (!memberGrp) {
-        return interaction.reply({
+        return interaction.editReply({
             embeds: [
                 createError(
                     `${newCaptain} ne fait pas parti du groupe **${grpName}** !`,
@@ -102,7 +104,7 @@ const transfert = async (interaction, options) => {
     const newMsgEmbed = new EmbedBuilder().setDescription(
         `${CHECK_MARK} ${newCaptain} est le nouveau capitaine du groupe **${grpName}** !`,
     );
-    await interaction.reply({ embeds: [newMsgEmbed] });
+    await interaction.editReply({ embeds: [newMsgEmbed] });
 };
 
 exports.transfert = transfert;
